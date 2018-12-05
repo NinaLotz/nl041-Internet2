c235854ec5b0bfe3ed6f0cf4a10c708761739ede
NinaLotz <45170100+NinaLotz@users.noreply.github.com>
2018-12-05 08:28:54 +0100

    Add files via upload

--- add.php
+++ add.php
@@ -4,28 +4,46 @@
     </head>
     
     <body>
+        <?php include('nav.inc'); ?>
+        
+        
+        
         <form action="index.php" method="get">
                 
-                Begrüßung:<br> <select name="Anrede" size="1">
+                Begrüßung:<br> 
+                <select name="Anrede" size="1">
                 <option>Guten Morgen</option>
                 <option>Mahlzeit</option>
                 <option>Guten Abend</option>
                 <option>Gute Nacht</option> <br>
                 </select>
                 <br>
-                Name:<br> <input type="text" name="name">
+                Name:<br> 
+                <input type="text" name="name">
                 <br>
+                Nachname: <br> 
+                <input type="text" name="nachname">     <br>
+                <h1>I Say YES! to... </h1>
         
-        <h1>I Say YES! to... </h1>
-        
-                <select name="Unfug 0" size="1">
-                <option>Unfug 0</option>
+                <select name="Adjektive" size="1">
+                <option>Kleine</option>
+                <option>Große</option>
+                <option>Dicke</option>
+                <option>Dünne</option>
+                
                 </select>
-                <select name="Unfug 1" size="1">
-                <option>Unfug 1</option>
+                <select name="Farben" size="1">
+                <option>Rote</option>
+                <option>Blaue</option>
+                <option>Gelbe</option>
+                <option>Grüne</option>
                 </select>
-                <select name="Unfug 2" size="1">
-                <option>Unfug 2</option>
+                <select name="Tiere" size="1">
+                <option>Giraffen</option>
+                <option>Zebras</option>
+                <option>Hunde</option>
+                <option>Katzen</option>
+                
                 </select>
                 <br>
             <input type="submit" value="Los geht´s!">
--- index.php
+++ index.php
@@ -1,12 +1,40 @@
+<?php include("config.ini")?>
+
+<?php $text =$_REQUEST["Adjektive"]." ".$_REQUEST["Farben"]." ".$_REQUEST["Tiere"]."\n";
+    file_put_contents($filename, $text, FILE_APPEND)
+    ?>
+
+<?php include('nav.inc'); ?>
+
+<?php
+    if(empty($_GET['nachname']))
+    { die("Kein Nachname übermittelt");}
+    ?>
+
+<!DOCTYPE html>
 <html>
-<head>
+    <head>
+        <title>Say YES!</title>
+    </head>
     
-</head>
+    <body>
+        
     
-<body>
-    <?php echo $_GET['Anrede']; ?>
-    <?php echo $_GET['name']; ?>
+            <?php echo $_GET['Anrede']; ?>
+            <?php echo $_GET['name']; ?>
+            <br>
+            
+           
+            <h1>I Say YES! to...</h1>
+            <br>
+            <?php $myText = file($filename, FILE_IGNORE_NEW_LINES);       
+            foreach ($myText as $value) 
+            { echo "<p>" . $value . "</p>";} 
+            ?>
+        
     
-</body>
+  
+    
+    </body>
 
 </html>
